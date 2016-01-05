74f138a2c3014c610f4a0a5ca8851900dabe5218
Jiwon Kim <jiwon.kim@totaljobsgroup.com>
2016-01-05 16:55:16 +0000

    Adding in Functionality :elephant:

---
+++ src/WeatherApi.Core/WeatherClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace WeatherApi.Core
+{
+    public class WeatherClient
+    {
+        private readonly IRestClient _restClient;
+
+        public WeatherClient(IRestClient restClient)
+        {
+            _restClient = restClient;
+        }
+
+        public WeatherInformation GetWeather()
+        {
+            return null;
+        }
+
+    }
+
+    public class WeatherInformation
+    {
+        
+    }
+}
---
+++ src/WeatherApi.Tests.Unit/WeatherClientTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WeatherApi.Core;
+using WeatherApi.Tests.Unit.Stubs;
+
+namespace WeatherApi.Tests.Unit
+{
+    [TestFixture]
+    public class WeatherClientTests
+    {
+        [Test]
+        public void Should_return_expected_weather_information()
+        {
+            // given
+            RestClientStub restClientStub = new RestClientStub();
+            WeatherClient weatherClient = new WeatherClient(restClientStub);
+            var expectedResult = new WeatherInformation();
+            
+            // when
+            var result = weatherClient.GetWeather();
+
+            // then
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
